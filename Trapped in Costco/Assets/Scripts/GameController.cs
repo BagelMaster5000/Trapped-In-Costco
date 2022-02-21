cbc1490729eabeb55515bdd881fb5641b246fee4
Sammy Mahmoudi <bagelmaster5001@gmail.com>
2022-02-21 14:39:39 -0600

    Item grabbing and throwing is now functional

--- Trapped in Costco/Assets/Scripts/GameController.cs
+++ Trapped in Costco/Assets/Scripts/GameController.cs
@@ -6,21 +6,36 @@ using UnityEngine.UI;
 
 public class GameController : MonoBehaviour
 {
+    [Header("Controls")]
+    [SerializeField] InputAction movementInput;
+    [SerializeField] InputAction clickInput;
+    [SerializeField] InputAction spinInput;
+
     public enum GameState {PLAYING, STARTMENU, PAUSEMENU, WINMENU};
     public GameState gameState = GameState.STARTMENU;
-    [SerializeField] InputAction movementInput;
 
     Timer timer;
 
+    [Header("Locations")]
     [SerializeField] Image background;
     [SerializeField] Location currentLocation;
     [SerializeField] Location[] allLocations;
     bool[] visitedLocations;
 
+    [Header("Item Holding")]
+    [SerializeField] Transform itemHoldLoc;
+    [SerializeField] float baseSpinSpeed = 1;
+    [SerializeField] float maxSpinSpeed = 25;
+    float curSpinSpeed;
+    [SerializeField] float spinSpeedStabilizationFactor = 20;
+    [SerializeField] LayerMask itemLayer;
+    [SerializeField] LayerMask backgroundLayer;
+    [SerializeField] float throwForce = 10;
     GameObject heldItem = null;
     GameObject[] itemLocationFolders;
     GameObject[][] itemsInLocations;
     Item[] allItems;
+
     int shoppingListLength;
     Item[] shoppingListItems;
     bool[] shoppingListCompletion;
@@ -43,6 +58,7 @@ public class GameController : MonoBehaviour
     public Action OnMoveLeft;
            
     public Action<GameObject, string> OnPickup;
+    public Action OnThrow;
     public Action OnPocket;
     public Action OnSmash;
     public Action OnSpin;
@@ -53,16 +69,42 @@ public class GameController : MonoBehaviour
     public Action OnGotCorrectItem;
     public Action OnGotWrongItem;
 
-
+    #region Setup
     private void Start()
+    {
+        curSpinSpeed = baseSpinSpeed;
+
+        InputsSetup();
+
+        ItemsInLocationFoldersSetup();
+        RefreshItems();
+    }
+
+    private void InputsSetup()
     {
         movementInput.performed += ctx => MoveInputRecieved(ctx.ReadValue<Vector2>());
         movementInput.Enable();
+        clickInput.performed += ctx => ClickInputRecieved();
+        clickInput.Enable();
+        spinInput.performed += ctx => Spin();
+        spinInput.Enable();
 
-        OnGamePause = () => { movementInput.Disable(); };
-        OnGameUnpause = () => { movementInput.Enable(); };
+        OnGamePause = () =>
+        {
+            movementInput.Disable();
+            clickInput.Disable();
+            spinInput.Disable();
+        };
+        OnGameUnpause = () =>
+        {
+            movementInput.Enable();
+            clickInput.Enable();
+            spinInput.Enable();
+        };
+    }
 
-
+    private void ItemsInLocationFoldersSetup()
+    {
         itemLocationFolders = new GameObject[allLocations.Length];
         itemsInLocations = new GameObject[allLocations.Length][];
         for (int l = 0; l < allLocations.Length; l++)
@@ -85,15 +127,36 @@ public class GameController : MonoBehaviour
                 itemsInLocations[l][i] = curItem;
             }
         }
-
-
-        RefreshItems();
     }
+    #endregion
 
     public void StartGame() { } // Sets game state to playing
     public void SetPauseGame(bool setPause) { } // Sets game state to either playing or paused
     public void RestartGame() { }
     public void ExitGame() { }
+
+    private void FixedUpdate()
+    {
+        if (heldItem != null)
+        {
+            SmoothMoveItemCloseToCamera();
+            IdleItemRotation();
+        }
+    }
+    private void SmoothMoveItemCloseToCamera()
+    {
+        heldItem.transform.localPosition /= 1.2f;
+        heldItem.transform.rotation = Quaternion.Lerp(heldItem.transform.rotation, Quaternion.Euler(0f, heldItem.transform.eulerAngles.y, 0f), 0.1f);
+    }
+    private void IdleItemRotation()
+    {
+        heldItem.transform.RotateAround(heldItem.transform.position, Vector3.up, curSpinSpeed);
+        if (curSpinSpeed > baseSpinSpeed)
+        {
+            curSpinSpeed = Mathf.Lerp(curSpinSpeed, baseSpinSpeed, 1 / spinSpeedStabilizationFactor);
+            if (curSpinSpeed < baseSpinSpeed + 0.1f) curSpinSpeed = baseSpinSpeed;
+        }
+    }
 
     /* Sets item texts in shopping list
      * Plays check animations for shopping list items that were found between now and the last refresh
@@ -165,16 +228,53 @@ public class GameController : MonoBehaviour
         }
     }
 
+    void ClickInputRecieved()
+    {
+        if (heldItem == null)
+            Pickup();
+        else
+            Throw();
+    }
     void Pickup()
     {
-        // Raycast to check for item collision
-        // Set held item
+        Ray ray = Camera.main.ScreenPointToRay(Mouse.current.position.ReadValue());
+        Debug.DrawLine(ray.origin, ray.origin + ray.direction * 100, Color.red, 100);
 
-        // On pickup action
+        if (Physics.Raycast(ray, out RaycastHit hit, 500, itemLayer))
+        {
+            heldItem = hit.transform.gameObject;
+            heldItem.GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
+            heldItem.GetComponent<Rigidbody>().velocity = Vector3.zero;
+            hit.transform.parent = itemHoldLoc;
+        }
+
+        OnPickup?.Invoke(heldItem, "Test Name");
+    }
+    void Throw()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Mouse.current.position.ReadValue());
+        Debug.DrawLine(Camera.main.transform.position, Mouse.current.position.ReadValue(), Color.red);
+
+        if (Physics.Raycast(ray, out RaycastHit hit, 500, backgroundLayer))
+        {
+            heldItem.transform.parent = itemLocationFolders[currentLocation.index].transform;
+
+            float spinForceMultiplier = Mathf.Clamp(curSpinSpeed / baseSpinSpeed, 1, 2);
+            heldItem.GetComponent<Rigidbody>().AddForce(ray.direction * throwForce * spinForceMultiplier, ForceMode.Impulse);
+
+            heldItem = null;
+        }
+
+        OnThrow?.Invoke();
     }
     void Pocket() { }
     void Smash() { }
-    void Spin() { }
+    void Spin()
+    {
+        if (heldItem == null) return;
+
+        curSpinSpeed = maxSpinSpeed;
+    }
 
     void Clap() { }
     void ThumbsUp() { }
