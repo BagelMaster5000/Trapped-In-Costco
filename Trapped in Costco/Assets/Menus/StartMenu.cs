3ae02edef0986fb62760f2f1403eb2509c5db582
Sammy Mahmoudi <bagelmaster5001@gmail.com>
2022-02-25 20:49:12 -0600

    Updated menus

--- Trapped in Costco/Assets/Menus/StartMenu.cs
+++ Trapped in Costco/Assets/Menus/StartMenu.cs
@@ -1,6 +1,11 @@
 public class StartMenu : Menu
 {
-    public void StartGame() => GameController.staticReference.StartGame();
+    public void StartGame()
+    {
+        GameController.staticReference.StartGame();
+
+        Disappear();
+    }
 
     public void ExitGame() => GameController.staticReference.ExitGame();
 }
