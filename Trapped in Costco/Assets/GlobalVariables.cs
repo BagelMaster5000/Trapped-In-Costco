ea2bfe9cd5a3735cb120a871fba816c663e35e26
Sammy Mahmoudi <bagelmaster5001@gmail.com>
2022-02-28 23:59:42 -0600

    Added settings and credits

---
+++ Trapped in Costco/Assets/GlobalVariables.cs
@@ -0,0 +1,5 @@
+public static class GlobalVariables
+{
+    public static bool showControls = true;
+    public static bool crtFilter = true;
+}
--- Trapped in Costco/Assets/Menus/StartMenu.cs
+++ Trapped in Costco/Assets/Menus/StartMenu.cs
@@ -1,8 +1,26 @@
+using HauntedPSX.RenderPipelines.PSX.Runtime;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.UI;
+
 public class StartMenu : Menu
 {
-    private void Awake()
+    [Header("Settings")]
+    [SerializeField] Toggle showControlsToggle;
+
+    [SerializeField] Toggle crtToggle;
+    [SerializeField] VolumeProfile globalVolumeProfile;
+
+    public override void Start()
     {
-        
+        base.Start();
+
+        showControlsToggle.SetIsOnWithoutNotify(GlobalVariables.showControls);
+        ApplyShowHideControls();
+
+        crtToggle.SetIsOnWithoutNotify(GlobalVariables.crtFilter);
+        ApplyCRTFilter();
     }
 
     public void StartGame()
@@ -13,4 +31,34 @@ public class StartMenu : Menu
     }
 
     public void ExitGame() => GameController.staticReference.ExitGame();
+
+    public void ToggleControls()
+    {
+        GlobalVariables.showControls = !GlobalVariables.showControls;
+        showControlsToggle.SetIsOnWithoutNotify(GlobalVariables.showControls);
+        ApplyShowHideControls();
+    }
+    void ApplyShowHideControls()
+    {
+        GameObject[] allControlTexts = GameObject.FindGameObjectsWithTag("ControlText");
+        for (int c = 0; c < allControlTexts.Length; c++)
+        {
+            if (allControlTexts[c].GetComponent<Image>())
+                allControlTexts[c].GetComponent<Image>().enabled = GlobalVariables.showControls;
+            if (allControlTexts[c].GetComponentInChildren<TextMeshProUGUI>())
+                allControlTexts[c].GetComponentInChildren<TextMeshProUGUI>().enabled = GlobalVariables.showControls;
+        }
+    }
+
+    public void ToggleCRTFilter()
+    {
+        GlobalVariables.crtFilter = !GlobalVariables.crtFilter;
+        crtToggle.SetIsOnWithoutNotify(GlobalVariables.crtFilter);
+        ApplyCRTFilter();
+    }
+    void ApplyCRTFilter()
+    {
+        globalVolumeProfile.TryGet(out QualityOverrideVolume v);
+        v.active = GlobalVariables.crtFilter;
+    }
 }
